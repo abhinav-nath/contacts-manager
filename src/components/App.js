40fed45499f33047d9ab27063355a36fd46fc064
Abhinav Nath <abhinavnath@ymail.com>
2022-12-02 02:23:08 +0530

    Persist contacts between page refreshes

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,9 @@ import ContactList from "./ContactList";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
 
   const addContactHandler = (contact) => {
     console.log(contact);
